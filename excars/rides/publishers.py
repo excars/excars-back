98444b4d2e029ee0416db742e4b1c109e2f264d1
Aleksei Maslakov <lesha.maslakov@gmail.com>
2018-12-14 20:36:09 +0200

    Clean up map generation

--- excars/rides/publishers.py
+++ excars/rides/publishers.py
@@ -1,20 +1,18 @@
-# pylint: skip-file
-
 import asyncio
+import typing
 
 from excars.ws import event
 
-from . import constants, factories, repositories, schemas
+from . import constants, entities, factories, repositories, schemas
 
 
 @event.publisher
 async def publish_map(request, ws, user):
-    redis_cli = request.app.redis
     frequency = 1
 
-    location_repo = repositories.UserLocationRepository(redis_cli)
-    profile_repo = repositories.ProfileRepository(redis_cli)
-    ride_repo = repositories.RideRepository(redis_cli)
+    location_repo = repositories.UserLocationRepository(request.app.redis)
+    profile_repo = repositories.ProfileRepository(request.app.redis)
+    ride_repo = repositories.RideRepository(request.app.redis)
 
     while True:
         await asyncio.sleep(frequency)
@@ -23,24 +21,7 @@ async def publish_map(request, ws, user):
         if not locations:
             continue
 
-        user_ride_uid = await ride_repo.get_ride_uid(user.uid)
-
-        map_items = []
-        for location in locations:
-            if location.user_uid == str(user.uid):
-                continue
-
-            profile = await profile_repo.get(location.user_uid)
-            if not profile:
-                continue
-
-            ride_uid = await ride_repo.get_ride_uid(profile.uid)
-            if ride_uid and ride_uid != user_ride_uid:
-                continue
-
-            has_same_ride = bool(user_ride_uid and (user_ride_uid == ride_uid))
-            map_items.append(factories.make_map_item(profile, location, has_same_ride))
-
+        map_items = await _prepare_map(user.uid, locations, profile_repo, ride_repo)
         if not map_items:
             continue
 
@@ -50,3 +31,30 @@ async def publish_map(request, ws, user):
         )
 
         await ws.send(schemas.MessageSchema().dumps(message).data)
+
+
+async def _prepare_map(
+        user_uid: str,
+        locations: typing.List[entities.UserLocation],
+        profile_repo: repositories.ProfileRepository,
+        ride_repo: repositories.RideRepository,
+):
+    user_ride_uid = await ride_repo.get_ride_uid(user_uid)
+
+    map_items = []
+    for location in locations:
+        if location.user_uid == str(user_uid):
+            continue
+
+        profile = await profile_repo.get(location.user_uid)
+        if not profile:
+            continue
+
+        ride_uid = await ride_repo.get_ride_uid(profile.uid)
+        if ride_uid and ride_uid != user_ride_uid:
+            continue
+
+        has_same_ride = bool(user_ride_uid and (user_ride_uid == ride_uid))
+        map_items.append(factories.make_map_item(profile, location, has_same_ride))
+
+    return map_items
--- excars/ws/tests/test_location_and_map.py
+++ excars/ws/tests/test_location_and_map.py
@@ -7,6 +7,23 @@ async def fake_coro():
     return
 
 
+@pytest.fixture
+def user_to_redis(test_cli):
+    async def wrapper(user):
+        await test_cli.app.redis.hmset_dict(
+            f'user:{user.uid}',
+            uid=str(user.uid),
+            name=user.get_name(),
+            avatar=user.avatar,
+            plate=user.plate,
+            role='driver',
+            dest_name='Porto Bello',
+            dest_lat=34.6709681,
+            dest_lon=33.0396582,
+        )
+    return wrapper
+
+
 @pytest.mark.require_db
 @pytest.mark.require_redis
 async def test_publish_location(test_cli, add_jwt, mocker):
@@ -17,40 +34,3 @@ async def test_publish_location(test_cli, add_jwt, mocker):
 
     with pytest.raises(asyncio.TimeoutError):
         await conn.receive_json(timeout=0.2)
-
-
-@pytest.mark.require_db
-@pytest.mark.require_redis
-@pytest.mark.skip
-async def test_publish_location_users_with_no_distance(test_cli, add_jwt, create_user, mocker):
-    mocker.patch('excars.ws.consumers.init', lambda *args, **kwargs: fake_coro())
-
-    user_1 = create_user(username='1')
-    conn_user_1 = await test_cli.ws_connect(
-        await add_jwt('/stream', user_uid=user_1.uid)
-    )
-    await conn_user_1.send_json({'data': {'longitude': 0, 'latitude': 0, 'course': -1}, 'type': 'LOCATION'})
-
-    user_2 = create_user(username='2')
-    conn_user_2 = await test_cli.ws_connect(
-        await add_jwt('/stream', user_uid=user_2.uid)
-    )
-    await conn_user_2.send_json({'data': {'longitude': 0.02, 'latitude': 0, 'course': -1}, 'type': 'LOCATION'})
-
-    user_3 = create_user(username='3')
-    conn_user_3 = await test_cli.ws_connect(
-        await add_jwt('/stream', user_uid=user_3.uid)
-    )
-
-    locations = await conn_user_2.receive_json()
-    assert locations['type'] == 'MAP'
-    assert len(locations['data']) == 2
-    uids = [i['user_uid'] for i in locations['data']]
-    assert str(user_1.uid) in uids
-    assert str(user_2.uid) in uids
-
-    user_data = locations['data'][0]['location']
-    assert {'user_uid', 'longitude', 'latitude', 'course'} == set(user_data.keys())
-
-    with pytest.raises(asyncio.TimeoutError):
-        await conn_user_3.receive_json(timeout=0.2)
